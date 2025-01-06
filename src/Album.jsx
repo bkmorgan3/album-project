fa0130d4b39b768373e6a3c29c303ff1188c5eef
Brad Morgan <bkmorgan3@gmail.com>
2025-01-06 13:18:01 -0800

    router, single item rough layout

--- src/Album.jsx
+++ src/Album.jsx
@@ -1,5 +1,5 @@
 import { truncate } from "./utils/truncate"
-
+import { Link } from 'react-router'
 export default function Album({album}) {
     
     return (
@@ -7,7 +7,7 @@ export default function Album({album}) {
             <img src={album['im:image'][2].label} />
             <div className="album-details">
                  <span className="artist-name">{truncate(album['im:artist'].label, 18)}</span>
-                 <p className="album-name">{truncate(album['im:name'].label,18)}</p>
+                <Link to={`albums/${album.id.attributes['im:id']}`} ><p className="album-name">{truncate(album['im:name'].label,18)}</p></Link>
             </div>
          </article>
     )
