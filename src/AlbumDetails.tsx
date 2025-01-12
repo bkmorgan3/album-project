c752ac7c12d9d53ce35f329c825d6eeb61bb0735
Brad Morgan <bkmorgan3@gmail.com>
2025-01-11 22:04:52 -0800

    add header to single fetch

--- src/AlbumDetails.tsx
+++ src/AlbumDetails.tsx
@@ -16,7 +16,13 @@ export default function AlbumDetails() {
 
     async function fetchAlbumDetails() {
         try {
-            const album = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`)
+            const album = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            })
             const json = await album.json()
             setAlbum(json.results)
             setIsLoading(false)
